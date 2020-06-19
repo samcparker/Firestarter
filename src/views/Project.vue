9051554af5b31d23d1fdca37cecabcff721335fd
Sam Parker <samcparker1999@gmail.com>
2020-06-19 10:54:35 +0100

    Able to write to Firestore now

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -7,22 +7,26 @@
             <v-list-item-title>{{ i }}</v-list-item-title>
           </v-list-item-content>
         </template>
-        <v-list-group sub-group v-for="(data, j) in collection" :key="j">
+        <v-list-group
+          sub-group
+          v-for="(doc, doc_id) in collection"
+          :key="doc_id"
+        >
           <template v-slot:activator>
             <v-list-item-content>
-              <v-list-item-title>{{ j }}</v-list-item-title>
+              <v-list-item-title>{{ doc_id }}</v-list-item-title>
             </v-list-item-content>
           </template>
-          <v-list-item v-for="(value, key) in data" :key="key">
+          <v-list-item v-for="(value, key) in doc" :key="key">
             <v-list-item-content>
               <v-row>
                 <v-col cols="2">{{ key }}</v-col>
 
                 <v-col cols="9">
                   <v-text-field
-                    @keypress="saveEdit(j, key, collection, $event)"
+                    @keypress="saveEdit(doc_id, key, collection, i, $event)"
                     v-model="edit_value"
-                    v-if="editing_id == j && editing_key == key"
+                    v-if="editing_id == doc_id && editing_key == key"
                     :value="value"
                   ></v-text-field>
 
@@ -30,9 +34,9 @@
                 </v-col>
 
                 <v-col cols="1">
-                  <v-row v-if="editing_id == j && editing_key == key" icon>
+                  <v-row v-if="editing_id == doc_id && editing_key == key" icon>
                     <v-btn icon>
-                      <v-icon v-on:click="saveEdit(j, key, collection)"
+                      <v-icon v-on:click="saveEdit(doc_id, key, collection, i)"
                         >mdi-check</v-icon
                       >
                     </v-btn>
@@ -41,7 +45,7 @@
                     </v-btn>
                   </v-row>
 
-                  <v-btn v-else v-on:click="editItem(j, key, value)" icon
+                  <v-btn v-else v-on:click="editItem(doc_id, key, value)" icon
                     ><v-icon>mdi-pencil-outline</v-icon></v-btn
                   >
                 </v-col>
@@ -69,52 +73,53 @@ export default {
   },
   methods: {
     editItem(id, key, value) {
-      this.edit_value = value;
+      // TODO : Convert back to object in future
+      if (typeof value == "object") {
+        this.edit_value = JSON.stringify(value);
+      } else {
+        this.edit_value = value;
+      }
+
       this.editing_id = id;
       this.editing_key = key;
-      
     },
     cancelEdit() {
       this.editing_id = null;
       this.editing_key = null;
       this.edit_value = null;
     },
-    saveEdit(id, key, collection, event) {
-      console.log(id);
+    saveEdit(id, key, collection, collection_name, event) {
       console.log(key);
-      console.log(this.edit_value);
+      let editVal = this.edit_value;
       if (event == null || event.key == "Enter") {
         collection[id][key] = this.edit_value;
         this.cancelEdit();
+        console.log(collection_name);
+        var out = {};
+        out[key] = editVal;
+        db.collection(collection_name)
+          .doc(id)
+          .set(out, { merge: true })
+          .then(() =>
+            console.log(
+              `Document successfully written!\nWritten ${editVal} to ${key}!`
+            )
+          )
+          .catch(function(error) {
+            console.log("Error writing document: ", error);
+          });
       }
       // else absorb
     },
     async getCollections() {
-      // db.collection("projects")
-      //   .get()
-      //   .then(function(querySnapshot) {
-      //     querySnapshot.forEach(function(doc) {
-      //       let data = doc.data();
-      //       let item = {};
-      //       item[doc.id] = data;
-      //       collections["projects"].push(item);
-      //     });
-      //   });
       var vue = this;
       db.collection("projects").onSnapshot(function(querySnapshot) {
-        // console.log("onsnap")
-        // collections = {};
-        // collections["projects"] = [];
         let collections = {};
         collections["projects"] = {};
 
         querySnapshot.forEach(function(doc) {
           let data = doc.data();
           collections["projects"][doc.id] = data;
-          // let data = doc.data();
-          // let item = {};
-          // item[doc.id] = data;
-          // collections["projects"].push(item);
         });
         vue.collections = collections;
         console.log(vue.collections);
